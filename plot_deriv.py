919b5dfef189aa2505a72e261f8896f7bcfc0b48
Katie <kbxyothers@gmail.com>
2023-08-20 22:17:20 -0600

    added ylim min and max

--- plot_deriv.py
+++ plot_deriv.py
@@ -25,9 +25,9 @@ def plot_graph(xdata, ydata, conc, pep, axis):
     axis.plot(smoothed_xdata, smoothed_ydata, label = conc)
     axis.set_title(pep)
     axis.set_xlabel("Time")
-    axis.set_ylim(bottom = -0.1, top = 0.1)
-    print(np.min(smoothed_ydata))
-    print(np.max(smoothed_ydata))
+    # axis.set_ylim(bottom = -0.1, top = 0.1)
+    # print(np.min(smoothed_ydata))
+    # print(np.max(smoothed_ydata))
     # axis.set_yscale("log")
     axis.legend(loc="upper right")
 
@@ -43,6 +43,8 @@ if __name__ == "__main__":
     concentrations_index = 0
     ax_outer_i = 0
     ax_inner_i = 0
+    miny = 1000
+    maxy = -1000
 
     import csv
     for csv_file in file_list:
@@ -62,12 +64,21 @@ if __name__ == "__main__":
 
         xprime, yprime = derivative(xdata, ydata)
 
+        if np.min(yprime) < miny:
+            miny = np.min(yprime)
+        if np.max(yprime) > maxy:
+            maxy = np.max(yprime)
+        print(f'miny = {miny} maxy = {maxy}')
+
+
         plot_graph(xprime, yprime, concentrations[concentrations_index], peptides[peptides_index], ax[ax_outer_i][ax_inner_i])
         counter += 1
         concentrations_index += 1
         # ax_inner_i += 1
 
         if counter % 4 == 0:
+            ax[ax_outer_i][ax_inner_i].set_ylim(bottom = miny, top = maxy)
+            break
 
             if peptides_index == 7:
                 peptides_index = 0
@@ -78,6 +89,8 @@ if __name__ == "__main__":
 
             concentrations_index = 0
             ax_inner_i += 1
+            miny = 1000
+            maxy = -1000
 
         if counter % 16 == 0:
             ax_inner_i = 0
